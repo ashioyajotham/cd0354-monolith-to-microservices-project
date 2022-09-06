eae1b79dfb0679746aa575d393010bde43605fd0
ashioyajotham <victorashioya960@gmail.com>
2022-09-06 11:58:03 +0300

    #

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a788bea4ad8f34404a5deb8d9e7b045f-1609604567.us-west-2.elb.amazonaws.com'
+  apiHost: 'http://a788bea4ad8f34404a5deb8d9e7b045f-1609604567.us-west-2.elb.amazonaws.com:80/api/v0'
 };
 
 /*
